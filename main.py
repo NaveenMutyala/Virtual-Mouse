d7e5df19226ddbd7c485b325ed09612a46c54d7e
Naveen kumar Mutyala <81841128+NaveenMutyala@users.noreply.github.com>
2022-12-12 09:35:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import cv2
 import numpy as np
 import HandTrackingModule as htm
-import time as t
+# import time as t
 import pyautogui as p
 import autopy
+
 
 
 
@@ -26,6 +27,7 @@ downF=False
 while True:
     # 1. Find hand Landmarks
     success, img = cap.read()
+    # print(success)
     img = detector.findHands(img)
     lmList, bbox = detector.findPosition(img)
     # 2. Get the tip of the index and middle fingers
@@ -39,35 +41,8 @@ while True:
         # print(fingers)
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR),(255, 0, 255), 2)
         # 4. Only Index Finger : Moving Mode
-        if fingers[0]==0 and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4]==0:
-            if not downF:
-                downF=True
-                p.mouseDown()
-            x1,y1=lmList[10][1:]
-            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
-            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
-            # 6. Smoothen Values
-            clocX = plocX + (x3 - plocX) / smoothening
-            clocY = plocY + (y3 - plocY) / smoothening
 
-            # 7. Move Mouse
-            p.moveTo(wScr - clocX, clocY)
-            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
-            plocX, plocY = clocX, clocY
-        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1:
-
-            l1, img, lineInfo = detector.findDistance(8, 12, img)
-            l2, img, lineInfo = detector.findDistance(12, 16, img)
-            length = l1 + l2
-            # print(l2)
-            if l2 < 40:
-                autopy.mouse.click(autopy.mouse.Button.RIGHT)
-        if fingers[0]==1 and fingers[1]:
-            l,img,lineInfo = detector.findDistance(4,8,img)
-            if l<40:
-                p.scroll(-20)
-            else:
-                p.scroll(20)
+        #mouse movement
         if fingers[1] == 1 and fingers[2] == 0:
             # 5. Convert Coordinates
 
@@ -86,8 +61,9 @@ while True:
             cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
             plocX, plocY = clocX, clocY
 
-        # 8. Both Index and middle fingers are up : Clicking Mode
-        if fingers[1] == 1 and fingers[2] == 1:
+        # 8. Both Index and middle fingers are up : Clicking Model
+        # left click
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 0:
             # 9. Find distance between fingers
             length, img, lineInfo = detector.findDistance(8, 12, img)
             print(length)
@@ -96,6 +72,41 @@ while True:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 15, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
 
+        # right click
+        if fingers[1] == 1 and fingers[2] == 1 and fingers[3] == 1:
+
+            l1, img, lineInfo = detector.findDistance(8, 12, img)
+            l2, img, lineInfo = detector.findDistance(12, 16, img)
+            length = l1 + l2
+            # print(l2)
+            if l1 < 40 and l2<40:
+                autopy.mouse.click(autopy.mouse.Button.RIGHT)
+
+        # dragging mouse
+        if fingers[0]==0 and fingers[1] == 0 and fingers[2] == 0 and fingers[3] == 0 and fingers[4]==0:
+            if not downF:
+                downF=True
+                p.mouseDown()
+            x1,y1=lmList[10][1:]
+            x3 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y3 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+            # 6. Smoothen Values
+            clocX = plocX + (x3 - plocX) / smoothening
+            clocY = plocY + (y3 - plocY) / smoothening
+
+            # 7. Move Mouse
+            p.moveTo(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 15, (255, 0, 255), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+        # scrolling
+        if fingers[0]==1 and fingers[1]==1:
+            l,img,lineInfo = detector.findDistance(4,8,img)
+            if l<40:
+                p.scroll(-20)
+            else:
+                p.scroll(20)
+    smoothening = 7
 
     cv2.imshow("Image", img)
     key = cv2.waitKey(10)
